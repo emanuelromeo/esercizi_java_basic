5aa1b3afe0ab9306953588280f030a2170ba10b6
emanuelromeo <emanuel.romeo@hotmail.com>
2024-11-13 03:08:52 +0100

    I tipi primitivi in Java - 2

---
+++ src/co/develhope/tipiPrimitivi/SecondoEsercizio.java
@@ -0,0 +1,13 @@
+package co.develhope.tipiPrimitivi;
+
+public class SecondoEsercizio {
+    public static void main(String[] args) {
+
+        System.out.println("Dichiarare 2 caratteri e stampare come risultato la combinazione dei due caratteri, affinché siano una stringa priva di spazi.");
+
+        char a = 'A';
+        char b = 'B';
+
+        System.out.println("La combinazione dei caratteri " + a + " e " + b + " è: " + a + b);
+    }
+}
