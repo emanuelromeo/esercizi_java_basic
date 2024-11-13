6d5eec4ca2680e06746ad6096496a148f785130f
emanuelromeo <emanuel.romeo@hotmail.com>
2024-11-13 03:24:25 +0100

    Casting - 2

--- src/casting/PrimoEsercizio.java
+++ src/casting/PrimoEsercizio.java
@@ -6,7 +6,7 @@ public class PrimoEsercizio {
         System.out.println("Scrivere uno snippet di codice che dati 2 numeri con la virgola (double) li sommi e li stampi senza virgola (int).");
 
         double a = 182.6;
-        double b = 23.1;
+        double b = 23.4;
 
         int sum = (int) (a + b);
         System.out.println(sum);
---
+++ src/casting/SecondoEsercizio.java
@@ -0,0 +1,14 @@
+package casting;
+
+public class SecondoEsercizio {
+    public static void main(String[] args) {
+
+        System.out.println("Dati due caratteri, restituire la somma dei loro valori ASCII");
+
+        char a = 'A';
+        char b = 'B';
+
+        int sum = a + b;
+        System.out.println("La somma dei valori ASCII di " + a + " e " + b + " è: " + sum);
+    }
+}
